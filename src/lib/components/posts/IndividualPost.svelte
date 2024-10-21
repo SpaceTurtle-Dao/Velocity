4f5b1669fdea21d3beeeb9e9144368f2e86a4b40
Jonathan Green <dev.jonathan.green@gmail.com>
2024-10-21 08:35:35 -0400

    added fix for fetching replies

--- src/lib/components/posts/IndividualPost.svelte
+++ src/lib/components/posts/IndividualPost.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
-    console.log("Loading it is!!")
-  import { onMount } from 'svelte';
-  import { _fetchEvents, info, event as aoEvent } from '$lib/ao/relay';
-  import Post from '$lib/components/posts/Post.svelte';
-  import Reply from '$lib/components/views/engagement/Reply.svelte';
-  import { Avatar, AvatarImage, AvatarFallback } from "$lib/components/ui/avatar";
+  console.log("Loading it is!!");
+  import { onMount } from "svelte";
+  import { _fetchEvents, info, event as aoEvent } from "$lib/ao/relay";
+  import Post from "$lib/components/posts/Post.svelte";
+  import Reply from "$lib/components/views/engagement/Reply.svelte";
+  import {
+    Avatar,
+    AvatarImage,
+    AvatarFallback,
+  } from "$lib/components/ui/avatar";
   import { CornerDownRight } from "lucide-svelte";
   import Nip92 from "$lib/handlers/NIP92.svelte";
   import Like from "$lib/components/views/engagement/Like.svelte";
@@ -12,25 +16,24 @@
   import Buy from "$lib/components/views/engagement/Buy.svelte";
   import Share from "$lib/components/views/engagement/Share.svelte";
   import { currentUser } from "$lib/stores/profile.store";
-  import { afterUpdate } from 'svelte';
-
+  import { afterUpdate } from "svelte";
 
   // Set the current URL
-  let url = window.location.href.split('/');
-  let id = url.pop() || '/';
-  let user = url.pop() || '/';
+  let url = window.location.href.split("/");
+  let id = url.pop() || "/";
+  let user = url.pop() || "/";
 
-  console.log("Answer it")
+  console.log("Answer it");
 
   // Set it as soon as pages update
   afterUpdate(() => {
-    let url = window.location.href.split('/');
-    id = url.pop() || '/';
-    user = url.pop() || '/';
+    let url = window.location.href.split("/");
+    id = url.pop() || "/";
+    user = url.pop() || "/";
   });
 
   console.log("user is this", user);
-  
+
   console.log("Id is this", id);
 
   let post: any = null;
@@ -40,37 +43,43 @@
 
   onMount(async () => {
     // Fetch the post
-    let postFilter = JSON.stringify([{
-      kinds: ["1"],
-      marker: "root",
-    //   ids: [id],
-    //   limit: 1
-    }]);
+    let postFilter = JSON.stringify([
+      {
+        kinds: ["1"],
+        marker: "root",
+        //   ids: [id],
+        //   limit: 1
+      },
+    ]);
     let postResults = await _fetchEvents(user, postFilter);
     if (postResults.length > 0) {
       post = postResults[0];
       _user = await info(post.From);
       profile = _user.Profile;
-      console.log("Post filter object", postFilter)
-      console.log("Post results", postResults)
+      console.log("Post filter object", postFilter);
+      console.log("Post results", postResults);
     }
 
-    // console.log("Post filter object", postFilter)
-    // console.log("Post results", postResults)
+    console.log("Post filter object", postFilter);
+    console.log("Post results", postResults);
 
-    // Fetch replies
-    // let replyFilter = JSON.stringify([{
-    //   kinds: ["1"],
-    //   '#e': [id],
-    //   limit: 100,
-    //   tags: {marker: ["reply"]}
-    // }]);
-    // replies = await _fetchEvents($currentUser.Process, replyFilter);
+    //Fetch replies
+    let replyFilter = JSON.stringify([
+      {
+        kinds: ["1"],
+        limit: 100,
+        tags: { marker: ["reply"] },
+      },
+      {
+        tags: { e: [id] },
+      },
+    ]);
+    replies = await _fetchEvents($currentUser.Process, replyFilter);
   });
 
   async function handleNewReply(replyEvent: any) {
     let newReply = replyEvent.detail;
-    
+
     // Add reply-specific tags
     let eTag = { name: "e", value: post.Id };
     let pTag = { name: "p", value: post.From };
@@ -94,7 +103,7 @@
             {#if profile?.picture}
               <AvatarImage src={profile.picture} alt="Avatar" />
             {:else}
-              <AvatarFallback>{profile?.name?.[0] || 'U'}</AvatarFallback>
+              <AvatarFallback>{profile?.name?.[0] || "U"}</AvatarFallback>
             {/if}
           </Avatar>
           <div>
@@ -104,9 +113,9 @@
         </div>
         <p class="mt-2 text-lg">{post.Tags.Content}</p>
         <div class="flex justify-between mt-4">
-          <Reply event={post} user={_user} on:newReply={handleNewReply}/>
+          <Reply event={post} user={_user} on:newReply={handleNewReply} />
           <Repost />
-          <Like _event={post}/>
+          <Like _event={post} />
           <Buy />
           <Share />
         </div>
