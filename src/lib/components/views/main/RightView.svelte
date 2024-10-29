5adbdcc373b9aa5fb58cb2619752801a391b30e2
SavageeAddie <nikhilkumarak_cse20@its.edu.in>
2024-10-30 01:46:44 +0530

    Replies working along with reposting the replies and replying to the repost

--- src/lib/components/views/main/RightView.svelte
+++ src/lib/components/views/main/RightView.svelte
@@ -2,6 +2,6 @@
     import UserList from "$lib/components/UserList/UserList.svelte";
 </script>
 
-<div class="flex justify-start pt-10 pl-10 w-1/3">
+<div class="flex justify-start overflow-auto pt-10 pl-10 w-1/3">
     <UserList />
 </div>
