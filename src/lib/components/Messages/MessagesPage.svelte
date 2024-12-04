ef64090cf5fc37840b792760fbb372f7ccd785e5
Rahees <ahdrahees.dev@gmail.com>
2024-12-04 22:44:29 +0530

    chore: message Page placeholder

--- src/lib/components/Messages/MessagesPage.svelte
+++ src/lib/components/Messages/MessagesPage.svelte
@@ -1,1 +1,10 @@
-<div></div>
+<script lang="ts">
+  import { MessagesSquare } from "lucide-svelte";
+</script>
+
+<div class="flex w-full justify-center mt-[320px] items-center">
+  <MessagesSquare class="text-muted-foreground size-16" />
+  <p class="text-muted-foreground text-center ml-3 text-lg">
+    Messaging feature coming soon...
+  </p>
+</div>
