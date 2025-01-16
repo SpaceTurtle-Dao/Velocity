b349bef974ede86993d2f6583eff61ef8be47c14
SavageeAddie <nikhilkumarak_cse20@its.edu.in>
2025-01-16 13:57:32 +0530

    User are able to see their profile just after registering

--- src/lib/components/views/profile/Profile.svelte
+++ src/lib/components/views/profile/Profile.svelte
@@ -31,7 +31,10 @@
 
   export let params: { address?: string } = {};
 
-  $: profile = $usersProfile.get(params?.address ?? "");
+  $: profile =
+    params?.address === $currentUser.address
+      ? $currentUser
+      : $usersProfile.get(params?.address ?? "");
 
   let activeTab: string = "posts";
   let events: Array<Event> = [];
@@ -268,19 +271,16 @@
         <div class="flex justify-between space-x-2">
           <p class="font-bold text-2xl">{profile.name}</p>
           {#if profile.address != $currentUser.address}
-          <Follow 
-            address={profile.address}
-          />
+            <Follow address={profile.address} />
           {:else}
-           <Button
+            <Button
               variant="outline"
               size="sm"
               class="text-primary rounded-full"
               on:click={toggleModal}
             >
               Edit Profile
-        </Button>
-
+            </Button>
           {/if}
         </div>
         <p class="text-muted-foreground">
