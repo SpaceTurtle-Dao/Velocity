174c40d08711f4e34e6416148a8ad0fe5e8b7db9
Nickk'z <contact.nikhil2002@gmail.com>
2025-05-27 00:23:41 +0530

    Search Page for Mobile

--- src/lib/components/views/main/MobileBottomNavBar.svelte
+++ src/lib/components/views/main/MobileBottomNavBar.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { currentUser } from "$lib/services/CurrentUser";
   import { isMobile } from "$lib/stores/is-mobile.store";
-  import { House, MessagesSquare, User, TestTube } from "lucide-svelte";
+  import { House, MessagesSquare, User, TestTube, Search } from "lucide-svelte";
   import { link, location } from "svelte-spa-router";
 </script>
 
@@ -16,11 +16,9 @@
       /></a
     >
 
-    <a href="/profile/{$currentUser?.address}" use:link
-      ><User
-        class={$location === `/profile/${$currentUser?.address}`
-          ? ""
-          : "text-muted-foreground"}
+    <a href="/search" use:link
+      ><Search
+        class={$location === "/search" ? "" : "text-muted-foreground"}
         size={28}
       /></a
     >
@@ -35,6 +33,15 @@
     <a href="/collections" use:link
       ><TestTube
         class={$location === "/collections" ? "" : "text-muted-foreground"}
+        size={28}
+      /></a
+    >
+
+    <a href="/profile/{$currentUser?.address}" use:link
+      ><User
+        class={$location === `/profile/${$currentUser?.address}`
+          ? ""
+          : "text-muted-foreground"}
         size={28}
       /></a
     >
